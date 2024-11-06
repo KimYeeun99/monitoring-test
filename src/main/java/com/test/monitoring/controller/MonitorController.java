5b1888a20007302c5022b036f6e142016bfedc68
KimYeeun99 <kyz0608@naver.com>
2024-11-06 14:10:08 +0900

    loki & promtail 추가

--- src/main/java/com/test/monitoring/controller/MonitorController.java
+++ src/main/java/com/test/monitoring/controller/MonitorController.java
@@ -1,19 +1,26 @@
 package com.test.monitoring.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @RestController
+@Slf4j
 @RequestMapping("/api")
 public class MonitorController {
     @GetMapping("/monitor1")
     public String test1(){
+        log.info("[Info] log정보입니다.");
         return "Monitoring Test1";
     }
 
     @GetMapping("/monitor2")
     public String test2(){
+        log.warn("[Info] log 위험 정보입니다.");
         return "Monitoring Test2";
     }
 }
